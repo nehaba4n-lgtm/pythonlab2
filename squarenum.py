7c453f08b9c5431470138eadbf9bba403f1103f1
nehaba4n-lgtm <nehaba4n@gmail.com>
2026-05-06 10:21:40 +0530

    Add files via upload

---
+++ squarenum.py
@@ -0,0 +1,3 @@
+numbers = [1,34,56,89.34,79]
+square_numbers = list(map(lambda x: x**2,numbers))
+print("Square numbers",square_numbers)
---
+++ sumofnum.py
@@ -0,0 +1,3 @@
+numbers = [1,3,4,56,67]
+sum_numbers =  reduce(lambda x,y:x+y,numbers)
+print("Sum of numbers",sum_numbers)
